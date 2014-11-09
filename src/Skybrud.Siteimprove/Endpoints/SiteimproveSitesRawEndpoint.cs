7786056ea6a9027e40296a386c2bfe14aac9b464
Anders Bjerner <abjerner@skybrud.dk>
2014-11-09 22:08:01 +0100

    Basic implementation for version 1.0 of the API

---
+++ src/Skybrud.Siteimprove/Endpoints/SiteimproveSitesRawEndpoint.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Specialized;
+using Skybrud.Social.Http;
+
+namespace Skybrud.Siteimprove.Endpoints {
+
+    public class SiteimproveSitesRawEndpoint {
+
+        #region Properties
+
+        public SiteimproveClient Client { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        internal SiteimproveSitesRawEndpoint(SiteimproveClient client) {
+            Client = client;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public SocialHttpResponse GetSite(int siteId) {
+            return Client.DoHttpGetRequest(SiteimproveClient.ApiUrl + "/sites/" + siteId);
+        }
+
+        public SocialHttpResponse GetSites() {
+            return GetSites(0, 0);
+        }
+
+        public SocialHttpResponse GetSites(int size) {
+            return GetSites(0, 0);
+        }
+
+        public SocialHttpResponse GetSites(int page, int pageSize) {
+
+            NameValueCollection query = new NameValueCollection();
+            if (page > 0) query.Add("page", page + "");
+            if (pageSize > 0) query.Add("page_size", pageSize + "");
+
+            return Client.DoHttpGetRequest(SiteimproveClient.ApiUrl + "/sites", query);
+
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ src/Skybrud.Siteimprove/SiteimproveClient.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Specialized;
+using System.Configuration;
+using System.Net;
+using System.Reflection;
+using Skybrud.Siteimprove.Endpoints;
+using Skybrud.Social;
+using Skybrud.Social.Http;
+
+namespace Skybrud.Siteimprove {
+    
+    public class SiteimproveClient {
+
+        public const string ApiUrl = "http://api.siteimprove.com/v1/";
+
+        #region Properties
+
+        public NetworkCredential Crendentials { get; private set; }
+
+        public SiteimproveSitesRawEndpoint Sites { get; private set; }
+        //public SiteimprovePagesRawEndpoint Pages { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        private SiteimproveClient() {
+            Sites = new SiteimproveSitesRawEndpoint(this);
+            //Pages = new SiteimprovePagesRawEndpoint(this);
+        }
+
+        #endregion
+
+        #region Static methods
+
+        /// <summary>
+        /// Initialize a new instance of <code>SiteimproveRawClient</code> based on values from the app settings.
+        /// </summary>
+        public static SiteimproveClient CreateFromConfig() {
+            string username = ConfigurationManager.AppSettings["SiteimproveUsername"];
+            string password = ConfigurationManager.AppSettings["SiteimprovePassword"];
+            return new SiteimproveClient {
+                Crendentials = new NetworkCredential(username, password)
+            };
+        }
+
+        /// <summary>
+        /// Initialize a new instance of <code>SiteimproveRawClient</code> from the specified username and password.
+        /// </summary>
+        /// <param name="username">The username of the Siteimprove account.</param>
+        /// <param name="password">The password of the Siteimprove account.</param>
+        public static SiteimproveClient CreateFromCredentials(string username, string password) {
+            return new SiteimproveClient {
+                Crendentials = new NetworkCredential(username, password)
+            };
+        }
+
+        #endregion
+
+        #region Member methods
+
+        /// <summary>
+        /// Make a HTTP GET request to the specified URL.
+        /// </summary>
+        /// <param name="url">The URL of the request.</param>
+        public SocialHttpResponse DoHttpGetRequest(string url) {
+            return DoHttpGetRequest(url, null);
+        }
+
+        /// <summary>
+        /// Make a HTTP GET request to the specified URL.
+        /// </summary>
+        /// <param name="url">The URL of the request.</param>
+        /// <param name="query">The query string of the request.</param>
+        public SocialHttpResponse DoHttpGetRequest(string url, NameValueCollection query) {
+            return new SocialHttpRequest {
+                Url = url,
+                QueryString = query,
+                Credentials = Crendentials,
+                Headers = new SocialHeaderCollection {
+                    Accept = "application/json"
+                }
+            }.GetResponse();
+        }
+
+        public SocialHttpResponse DoHttpGetRequest(string url, object query) {
+
+            NameValueCollection nvc = new NameValueCollection();
+
+            // Get all public properties from the specified data object
+            PropertyInfo[] properties = query.GetType().GetProperties();
+
+            foreach (PropertyInfo property in properties) {
+                object value = property.GetValue(query, new object[0]);
+                nvc[property.Name] = value == null ? null : value + "";
+            }
+
+            return DoHttpGetRequest(url, nvc);
+
+        }
+
+        public NameValueCollection ObjectToNameValueCollection(object query) {
+
+            NameValueCollection nvc = new NameValueCollection();
+
+            foreach (PropertyInfo property in query.GetType().GetProperties()) {
+                object value = property.GetValue(query, new object[0]);
+                nvc[property.Name] = value == null ? null : value + "";
+            }
+
+            return nvc;
+
+        }
+
+        #endregion
+
+    }
+
+}
