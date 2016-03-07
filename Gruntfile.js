033ea625a744ac4930021d19299f8604e988f478
Anders Bjerner <abjerner@skybrud.dk>
2016-03-07 12:41:39 +0100

    Build setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,48 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-	grunt.initConfig({
-		hub: {
-			release: {
-				src: ['siteimprove.js'],
-				tasks: ['release']
-			}
-		}
-	});
+    var path = require('path');
 
-	// Add repo tasks
-	grunt.loadNpmTasks('grunt-hub');
-	grunt.registerTask('release', ['hub:release']);
-	grunt.registerTask('default', ['hub:release']);
+    // Load the package JSON file
+    var pkg = grunt.file.readJSON('package.json');
+
+    // Load information about the assembly
+    var assembly = grunt.file.readJSON('src/' + pkg.name + '/Properties/AssemblyInfo.json');
+
+    // Get the version of the package
+    var version = assembly.informationalVersion ? assembly.informationalVersion : assembly.version;
+
+    grunt.initConfig({
+        pkg: pkg,
+        nugetpack: {
+            release: {
+                src: 'src/' + pkg.name + '/' + pkg.name + '.csproj',
+                dest: 'releases/nuget/'
+            }
+        },
+        zip: {
+            release: {
+                router: function (filepath) {
+                    return path.basename(filepath);
+                },
+                src: [
+					'src/' + pkg.name + '/bin/Release/Newtonsoft.Json.dll',
+					'src/' + pkg.name + '/bin/Release/Newtonsoft.Json.xml',
+					'src/' + pkg.name + '/bin/Release/Skybrud.Social.dll',
+					'src/' + pkg.name + '/bin/Release/Skybrud.Social.xml',
+					'src/' + pkg.name + '/bin/Release/' + pkg.name + '.dll',
+					'src/' + pkg.name + '/bin/Release/' + pkg.name + '.xml',
+					'src/' + pkg.name + '/LICENSE.txt'
+                ],
+                dest: 'releases/github/' + pkg.name + '.v' + version + '.zip'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-nuget');
+    grunt.loadNpmTasks('grunt-zip');
+
+    grunt.registerTask('release', ['nugetpack', 'zip']);
+
+    grunt.registerTask('default', ['release']);
 
 };
