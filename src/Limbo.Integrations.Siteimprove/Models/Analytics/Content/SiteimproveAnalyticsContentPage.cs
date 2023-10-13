f76fe881899d61a5f881dfbd34455186bb3dda28
Anders Bjerner <abjerner@skybrud.dk>
2023-10-13 14:58:10 +0200

    Changing "Id" property type from "int" to "ulong"
    Since the initial release of the V2 API, IDs have grown quite a lot, and now needs to be represented by a "ulong".

--- src/Limbo.Integrations.Siteimprove/Models/Analytics/Content/SiteimproveAnalyticsContentPage.cs
+++ src/Limbo.Integrations.Siteimprove/Models/Analytics/Content/SiteimproveAnalyticsContentPage.cs
@@ -3,6 +3,9 @@ using Skybrud.Essentials.Json.Extensions;
 
 namespace Limbo.Integrations.Siteimprove.Models.Analytics.Content {
 
+    /// <summary>
+    /// Class representing a content page in the <strong>Analytics</strong> endpoint.
+    /// </summary>
     public class SiteimproveAnalyticsContentPage : SiteimproveObject {
 
         #region Properties
@@ -10,7 +13,7 @@ namespace Limbo.Integrations.Siteimprove.Models.Analytics.Content {
         /// <summary>
         /// Gets the ID of the page.
         /// </summary>
-        public int Id { get; }
+        public ulong Id { get; }
 
         /// <summary>
         /// Gets the title of the page.
@@ -52,7 +55,7 @@ namespace Limbo.Integrations.Siteimprove.Models.Analytics.Content {
         #region Constructors
 
         private SiteimproveAnalyticsContentPage(JObject obj) : base(obj) {
-            Id = obj.GetInt32("id");
+            Id = obj.GetUInt64("id");
             Title = obj.GetString("title");
             Url = obj.GetString("url");
             BounceRate = obj.GetFloat("bounce_rate");
@@ -70,8 +73,13 @@ namespace Limbo.Integrations.Siteimprove.Models.Analytics.Content {
 
         #region Static methods
 
-        public static SiteimproveAnalyticsContentPage Parse(JObject obj) {
-            return obj == null ? null : new SiteimproveAnalyticsContentPage(obj);
+        /// <summary>
+        /// Parses the specified <paramref name="json"/> object into an instance of <see cref="SiteimproveAnalyticsContentPage"/>.
+        /// </summary>
+        /// <param name="json">The JSON object representing the content page.</param>
+        /// <returns>An instance of <see cref="SiteimproveAnalyticsContentPage"/>.</returns>
+        public static SiteimproveAnalyticsContentPage Parse(JObject json) {
+            return json == null ? null : new SiteimproveAnalyticsContentPage(json);
         }
 
         #endregion
